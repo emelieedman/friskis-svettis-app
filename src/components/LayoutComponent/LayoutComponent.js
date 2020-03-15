68d08ce7b2213ae6c6c46b76d9a7d425d360423f
Emelie Edman <emelie.edman@hyperisland.se>
2020-03-15 21:39:39 +0100

    created HeaderComponent

--- src/components/LayoutComponent/LayoutComponent.js
+++ src/components/LayoutComponent/LayoutComponent.js
@@ -1,9 +1,10 @@
 import React from "react";
+import HeaderComponent from "../HeaderComponent/HeaderComponent";
 
 function LayoutComponent() {
     return (
         <div>
-            
+            <HeaderComponent />
         </div>
     )
 }
