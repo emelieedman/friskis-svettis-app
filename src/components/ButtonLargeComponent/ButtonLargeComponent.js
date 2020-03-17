0b43bf1d7ec13ce08dfe5c3c72c3ba11fd37d788
Emelie Edman <emelie.edman@hyperisland.se>
2020-03-17 12:19:45 +0100

    changed buttons into <Link>

--- src/components/ButtonLargeComponent/ButtonLargeComponent.js
+++ src/components/ButtonLargeComponent/ButtonLargeComponent.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const ButtonLargeComponent = (props) => {
     return (
-        <Link to={props.path}><button className={styles.buttonLarge}>{props.text}</button></Link>
+        <Link to={props.path} className={styles.buttonLarge}>{props.text}</Link>
     )
 }
 
