ef8b276beafd8e2b06bf74a86265df124d26fd43
Emelie Edman <emelie.edman@hyperisland.se>
2020-03-18 21:33:36 +0100

    fixed the search function in PickGymComponent

--- src/components/InputFieldComponent/InputFieldComponent.js
+++ src/components/InputFieldComponent/InputFieldComponent.js
@@ -5,7 +5,7 @@ const InputFieldComponent = (props) => {
     return (
         <form className={styles.wrapper}>
             <label className={styles.label} htmlFor="search">{props.label}</label>
-            <input className={styles.input} type="text" label="search" />
+            <input className={styles.input} type="text" value={props.value} onChange={props.onChange} label="search" />
         </form>
     )
 }
